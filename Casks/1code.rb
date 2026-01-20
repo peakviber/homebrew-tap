275ff4cc48225d44ef5b3c03cc12fa9594460857
peakviber <160327127+peakviber@users.noreply.github.com>
2026-01-20 20:16:28 +0800

    Fix style issues in 1code cask

--- Casks/1code.rb
+++ Casks/1code.rb
@@ -5,7 +5,7 @@ cask "1code" do
   sha256 arm:   "a52b281303078fedb516f78c41f5190feba589644559f938034da20b26c7d0c9",
          intel: "d2cba0db75f40bcf5468359b90ce3ebbcef00fbc50cdc1617d8acc92e895fa6b"
 
-  url "https://cdn.21st.dev/releases/desktop/1Code-#{version}#{arch.empty? ? '' : '-' + arch}.dmg",
+  url "https://cdn.21st.dev/releases/desktop/1Code-#{version}#{"-" + arch unless arch.empty?}.dmg",
       verified: "cdn.21st.dev/releases/desktop/"
   name "1Code"
   desc "Better UI app for running code agents in parallel"
